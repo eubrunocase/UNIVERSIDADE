cf2cf64f9d43ab9e665bd44849aa3dda0617e7c1
Guimasz <gui10asouza@gmail.com>
2023-12-01 23:15:25 -0300

    Lógica das reservas está funcional

--- src/auxiliar/SituacaoReserva.java
+++ src/auxiliar/SituacaoReserva.java
@@ -1,25 +1,7 @@
-/**
- * A enumeração `SituacaoReserva` representa os possíveis estados de uma reserva no sistema de reserva da Ucsal.
- * Pode ser APROVADA ou REPROVADA.
- *
- * @author BRUNO CASÉ and ZAIRA DUTRA
- * @version 1.0
- * @since 21/11/2023
- */
 package auxiliar;
 
-/**
- * Enumeração que define os possíveis estados de uma reserva.
- */
-enum SituacaoReserva {
-
-    /**
-     * Indica que a reserva foi aprovada.
-     */
+ enum SituacaoReserva {                                                  // Powered by: BRUNO CASÉ and ZAIRA DUTRA
+                                                                          //Last att: 12:58 pm  (21/11/2023)
     APROVADA,
-
-    /**
-     * Indica que a reserva foi reprovada.
-     */
     REPROVADA
 }
--- src/entities/Laboratorio.java
+++ src/entities/Laboratorio.java
@@ -1,120 +1,55 @@
-/**
- * A classe `Laboratorio` representa um laboratório no sistema de reserva de laboratórios da Ucsal.
- * Possui atributos como id, descrição, capacidade e status.
- *
- * @author Bruno Casé e Zaira Dutra
- * @version 1.0
- * @since 21/11/2023
- */
 package entities;
 
-/**
- * Classe que representa um laboratório.
- */
-public class Laboratorio {
-
-    // Atributos
+public class Laboratorio {                                // Powered by: BRUNO CASÉ and ZAIRA DUTRA
+    //Last att: 12:58 pm  (21/11/2023)
     private int id;
     private String descricao;
     private int capacidade;
-    private boolean status;
+    private boolean status;                             // ATRIBUTOS
 
-    /**
-     * Construtor da classe `Laboratorio`.
-     *
-     * @param id         O id do laboratório.
-     * @param descricao  A descrição do laboratório.
-     * @param capacidade A capacidade do laboratório.
-     * @param status     O status do laboratório.
-     */
-    public Laboratorio(int id, String descricao, int capacidade, boolean status) {
+
+    public Laboratorio(int id, String descricao, int capacidade, boolean status) {   // COSNTRUTOR
         this.id = id;
         this.descricao = descricao;
         this.capacidade = capacidade;
         this.status = status;
     }
 
-    /**
-     * Obtém a capacidade do laboratório.
-     *
-     * @return A capacidade do laboratório.
-     */
-    public int getCapacidade() {
+    public int getCapacidade() {              // METODOS ACESSORES E MODIFICADORES
         return capacidade;
     }
 
-    /**
-     * Define a capacidade do laboratório.
-     *
-     * @param capacidade A nova capacidade do laboratório.
-     */
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
 
-    /**
-     * Obtém a descrição do laboratório.
-     *
-     * @return A descrição do laboratório.
-     */
     public String getDescricao() {
         return descricao;
     }
 
-    /**
-     * Define a descrição do laboratório.
-     *
-     * @param descricao A nova descrição do laboratório.
-     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
-    /**
-     * Obtém o id do laboratório.
-     *
-     * @return O id do laboratório.
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * Define o id do laboratório.
-     *
-     * @param id O novo id do laboratório.
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-
-    /**
-     * Verifica se o laboratório está ativo.
-     *
-     * @return True se estiver ativo, false se estiver inativo.
-     */
-    public boolean isStatus() {
-
+    public boolean getStatus() {
         return status;
     }
 
-    /**
-     * Define o status do laboratório.
-     *
-     * @param status O novo status do laboratório.
-     */
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    /**
-     * Representação textual do laboratório.
-     *
-     * @return Uma string representando o laboratório.
-     */
-    @Override
-    public String toString() {
+    public String toString() {                         // TO STRING
         return "Laboratorio[id=" + id + ", descricao=" + descricao + ", capacidade=" + capacidade + ", status=" + status + "]";
     }
 }
+
+
+
